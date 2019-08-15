445f820bf652918ed29a0606c232a269fa094efd
Adrien RICHARD <richardadrien0@gmail.com>
2019-08-15 22:39:50 +0200

    improuvements

--- js/main.js
+++ js/main.js
@@ -1,17 +1,41 @@
-function main() {
-    console.info("Lanched process");
 
+//vars
+var filecontent = undefined;
+var stats = {
+    days =[],
+    totalDays =[],
+    hours =[],
+    totalHours =[],
+    minutes =[],
+    totalMinutes =[],
+    seconds =[],
+    totalSeconds =[]
+}
+
+function readFile() {
+    console.info("reading file");
     var file = document.getElementById("fileForUpload").files[0];
     if (file) {
         var reader = new FileReader();
         reader.readAsText(file, "UTF-8");
         reader.onload = function (evt) {
-            var rawJSON = evt.target.result;
+            console.info("Parsiong File")
             var parsedJSON = JSON.parse(evt.target.result);
+            filecontent = parsedJSON;
             console.info(parsedJSON);
-            // document.getElementById("fileContents").innerHTML = evt.target.result;
-            document.getElementById("totaldays").innerText = parsedJSON.days.length + " days found"
-            extractData(parsedJSON);
+
+            console.info("Extracting data");
+            parsedJSON.days.forEach(element => {
+                days.push(element.date);
+                hours.push(element.grand_total.hours);
+                minutes.push(element.grand_total.hours * 60 + element.grand_total.minutes)
+                seconds.push(element.grand_total.total_seconds)
+            });
+
+            document.getElementById("totaldays").innerHTML =
+                "File is valid ! " + parsedJSON.days.length + " days found " +
+                "<button type=\"button\" onclick=\"getStats();\">get stats</button>"
+            console.info("File parsed & data extracted");
         }
         reader.onerror = function (evt) {
             document.getElementById("fileContents").innerHTML = "error reading file";
@@ -19,30 +43,12 @@ function main() {
     }
 }
 
-function extractData(myJSON) {
-    console.info("Extracting data");
-
-    //vars
-    var days = []
-    var hours = [];
-    var minutes = [];
-
-    //functions
-
-    myJSON.days.forEach(element => {
-        days.push(element.date);
-        hours.push(element.grand_total.hours);
-        minutes.push(element.grand_total.hours * 60 + element.grand_total.minutes)
-    });
-    console.log(hours);
-    console.log(minutes);
-
-
-    mostCoded(myJSON);
+function getStats() {
     calcAverage(minutes);
-
-
-    //charts
+    totalDays = this.total(days);
+    totalHours = this.total(hours);
+    totalMinutes = this.total(minutes);
+    totalSeconds = this.total(seconds);
     var chart = c3.generate({
         bindto: '#chart',
         data: {
@@ -57,6 +63,15 @@ function calcAverage(numbers) {
     return (sum / sum.numbers);
 }
 
+
 function mostCoded(alldays) {
-    Math.max.apply(Math, alldays.map(function (o) { return o.grand_total.total_seconds; }))
+    //    Math.max.apply(Math, alldays.map(function (o) { return o.grand_total.total_seconds; }))
+}
+
+function total(numbers) {
+    var tmp;
+    numbers.forEach(element => {
+        tmp += element
+    });
+    return tmp;
 }
